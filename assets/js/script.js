dd36021cf382db205a4161d33917c72c6c3eee00
Paulo Henrique Reis <87020059+paulooliveirv@users.noreply.github.com>
2022-03-14 07:35:31 -0300

    avancando com css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
-const fetchConselho = () => {
-    const requisicao = fetch(`https://api.adviceslip.com/advice`)
+fetch(`https://api.adviceslip.com/advice`)
         .then(response =>{
             return response.json();
         }).then(jsonBody => {
-            document.getElementById("conselho_id").innerHTML = jsonBody.slip.id;
+            document.getElementById("conselho_id").innerHTML = "ADVICE #"  + jsonBody.slip.id;
             document.getElementById("conselho_frase").innerHTML = jsonBody.slip.advice;
             console.log(jsonBody.slip.id)
-        })
-};
-fetchConselho();
+        });
+
+        
