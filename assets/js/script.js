01dcd43913de61a5d70ed6ee7e4449af26fb2022
Paulo Henrique Reis <87020059+paulooliveirv@users.noreply.github.com>
2022-03-14 08:11:57 -0300

    posicionando elementos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
-fetch(`https://api.adviceslip.com/advice`)
+consulta = fetch(`https://api.adviceslip.com/advice`)
         .then(response =>{
             return response.json();
         }).then(jsonBody => {
-            document.getElementById("conselho_id").innerHTML = "ADVICE #"  + jsonBody.slip.id;
+            document.getElementById("conselho_id").innerHTML ="ADVICE #" + jsonBody.slip.id;
             document.getElementById("conselho_frase").innerHTML = jsonBody.slip.advice;
             console.log(jsonBody.slip.id)
         });
-
-        
